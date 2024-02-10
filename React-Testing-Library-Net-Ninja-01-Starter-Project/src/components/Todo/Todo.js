56eae55f8e97b41827c4f46eaa1400b55d892381
Muano <muano.m365@gmail.com>
2024-02-10 17:46:47 +0200

    Explored the getByText, getByRole, getByTitle, getByTestId, findByText, queryByText and getAllByRole query methods

--- React-Testing-Library-Net-Ninja-01-Starter-Project/src/components/Todo/Todo.js
+++ React-Testing-Library-Net-Ninja-01-Starter-Project/src/components/Todo/Todo.js
@@ -10,7 +10,7 @@ function Todo() {
 
     return (
         <div className="todo">
-            <Header title="Todo" />
+            <Header title="WebCV" />
             <AddInput 
                 setTodos={setTodos}
                 todos={todos}
