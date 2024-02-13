4b5667aedb50611a6fdf8ac5172f7866efd95f97
Muano <muano.m365@gmail.com>
2024-02-13 13:35:07 +0200

    Added the test to see if input element's value is empty when add button is clicked in the AddInput component

--- React-Testing-Library-Net-Ninja-01-Starter-Project/src/components/AddInput/__test__/AddInput.test.js
+++ React-Testing-Library-Net-Ninja-01-Starter-Project/src/components/AddInput/__test__/AddInput.test.js
@@ -7,13 +7,27 @@ describe("AddInput", () => {
     test("should render input element", () => {
         render(<AddInput todos={[]} setTodos={mockSetTodos}></AddInput>);
         const inputElement = screen.getByPlaceholderText(/Add a new task here.../i);
+        
         expect(inputElement).toBeInTheDocument;
     })
 
     test("should be able to type in input", () => {
         render(<AddInput todos={[]} setTodos={mockSetTodos}></AddInput>);
         const inputElement = screen.getByPlaceholderText(/Add a new task here.../i);
-        fireEvent.change(inputElement, { target: {value: "Go grocery shopping"}})
+
+        fireEvent.change(inputElement, { target: {value: "Go grocery shopping"}});
+
         expect(inputElement.value).toBe("Go grocery shopping");
+    })
+
+    test("should have empty input when add button is clicked", () => {
+        render(<AddInput todos={[]} setTodos={mockSetTodos}></AddInput>);
+        const inputElement = screen.getByPlaceholderText(/Add a new task here.../i);
+        const buttonElement = screen.getByRole("button", { name: /Add/i });
+
+        fireEvent.change(inputElement, { target: {value: "Go grocery shopping"}});
+        fireEvent.click(buttonElement);
+
+        expect(inputElement.value).toBe("");
     })
 })
