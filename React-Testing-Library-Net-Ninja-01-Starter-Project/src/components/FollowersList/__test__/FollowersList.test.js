962b3402a2231ef2d33979b9e13925ab712aa581
Muano <muano.m365@gmail.com>
2024-02-15 11:29:32 +0200

    Added test to see if the multiple items are being rendered

--- React-Testing-Library-Net-Ninja-01-Starter-Project/src/components/FollowersList/__test__/FollowersList.test.js
+++ React-Testing-Library-Net-Ninja-01-Starter-Project/src/components/FollowersList/__test__/FollowersList.test.js
@@ -16,4 +16,10 @@ describe("FollowersList", () => {
         const followerDivElement = await screen.findByTestId(/follower-item-0/i);
         expect(followerDivElement).toBeInTheDocument();
     })
+
+    test("should render multiple follower items", async () => {
+        render(<MockFollowersList></MockFollowersList>);
+        const followerDivElements = await screen.findAllByTestId(/follower-item/i);
+        expect(followerDivElements.length).toBe(5);
+    })
 })
